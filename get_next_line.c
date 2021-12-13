c8b94da8fc019548c6140e8fd0e1e928fcc56317
Olli Airola <oairola@c1r6p9.hive.fi>
2021-12-13 14:53:52 +0200

    saving

---
+++ get_next_line.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oairola <oairola@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/13 10:41:20 by oairola           #+#    #+#             */
+/*   Updated: 2021/12/13 13:57:37 by oairola          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./libft.h"
+#include "get_next_line.h"
+
+/*
+void input reader(fd, line)
+{
+	read from input
+	write to output
+}
+
+int read function(int fd, char **line, pointer to static char)
+{
+	while line[i] = \n or EOF
+		go through char by char
+		start writing when line[i] is not static char anymore!!
+		also save into static char to where to start next line reading
+		if \n
+			flag = 0
+		if EOF
+			flag = -1
+	return flag
+}
+
+get next line(fd, line)
+{
+	if fd 1
+		input reader
+	int = read function : read one line and save to line variable without the \n
+		return -1 if couldnt read
+	return 1 if still stuff left, 0 if end of file, -1 if couldnt read
+}
+*/
+
+void	input_reader(fd)
+{
+	int	ret;
+	char buf[BUF_SIZE + 1];
+
+	while (ret = read(0, buf, BUF_SIZE))
+		ft_putstr(buf);
+}
+
+int	read_function(int fd, char **line, char **strsaver)
+{
+	int	i;
+
+	i = 0;
+	while (*line[i] != '\n' && *line[i])
+	{
+		if strsaver[i] != line[i]
+			strsaver[i++] = line[i];
+		i++;
+	}
+	if (*line[i] == '\n')
+		return (1);
+	else
+		return (0);
+}
+
+int get_next_line(const int fd, char **line)
+{
+	static char *strsaver;
+	int flag;
+	
+	if (fd == 1)
+		input_reader(fd);
+	else
+		flag = read_function(fd, line, &strsaver);
+	return (flag);
+}
---
+++ main.c
@@ -0,0 +1,82 @@
+#include "get_next_line.h"
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+	char *s;
+	int x;
+
+	int fd = open("todo.txt", O_RDONLY);
+	int fd1 = open("toinenTesti.txt", O_RDONLY);
+	int fd2 = open("test1.txt", O_RDONLY);
+	int fd3 = open("test2.txt", O_RDONLY);
+	int fd4 = open("empty.txt", O_RDONLY);
+	int fd5 = open("emptyline.txt", O_RDONLY);
+	int fd6 = open("bible.txt", O_RDONLY);
+	
+
+	printf("-------\nTesting negative, supposed -1, fd:\n%d\n", get_next_line(-5, &s));
+	printf("-------\nTesting NULL, supposed -1, **line:\n%d\n", get_next_line(1, 0));
+	printf("-------\nBasic tests, sisalto 1:na ja 0 kun ei uutta linea:\n");
+
+	for (int i = 0; i < 2; i++)
+	{
+		x = get_next_line(fd, &s);
+		printf("%d, %s\n", x, s);
+	}
+	printf("\n");
+
+
+
+	while (1)
+	{
+		x = get_next_line(fd1, &s);
+		
+		printf("%d, %s\n", x, s);
+		if (x <= 0)
+			break ;
+	}
+
+	printf("-------\nTesting empty file, supposed 0,:\n");
+	x = get_next_line(fd4, &s);
+	printf("%d, %s\n", x, s);
+
+	printf("-------\nTesting file with just empty line, supposed 1 ja sit 0:\n");
+	x = get_next_line(fd5, &s);
+	printf("%d, %s\n", x, s);
+	x = get_next_line(fd5, &s);
+	printf("%d, %s\n", x, s);	
+
+	printf("-------\nTesting calling diff fd one after another:\n");
+	get_next_line(fd2, &s);
+	printf("supposed first call, %s\n",s);
+	get_next_line(fd3, &s);
+	printf("second call, %s\n",s);
+	get_next_line(fd2, &s);
+	printf("third call, %s\n",s);
+	get_next_line(fd3, &s);
+	printf("fourth call, %s\n",s);
+
+	printf("-------\nBIBLE:\n-----------");
+	int i = 0;
+	while (1)
+	{
+		x = get_next_line(fd6, &s);
+		
+		printf("%d, %s\n", i, s);
+		if (x <= 0)
+			break ;
+		i++;
+	}
+
+	printf("-------\nTesting stdin:\n");
+	while (1)
+	{
+		get_next_line(1, &s);
+		printf("%s\n",s);
+	}
+
+
+
+}
